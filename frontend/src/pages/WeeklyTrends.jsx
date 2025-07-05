d1ddd81b801b54c70dcf96b2e0d67efe917083d8
victor juma <77642020+vic657@users.noreply.github.com>
2025-07-05 14:03:21 +0300

    Update WeeklyTrends.jsx

--- frontend/src/pages/WeeklyTrends.jsx
+++ frontend/src/pages/WeeklyTrends.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "../axios";
-import BackButton from "../components/BackButton";
+import BackButton from "../Components/BackButton";
 
 
 import {
