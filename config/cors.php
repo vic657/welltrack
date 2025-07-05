1f0c68d21b34939f01a57736f7a560a21eddfe30
victor juma <77642020+vic657@users.noreply.github.com>
2025-07-05 16:10:31 +0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://welltrack-production.up.railway.app'],
+    'allowed_origins' => ['https://welltrack-production.up.railway.app','https://welltrack.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
