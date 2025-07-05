d1f8d233c6070b32c3bdc911172283846520a16a
victor juma <77642020+vic657@users.noreply.github.com>
2025-07-05 14:53:44 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/health', fn() => response()->json(['status' => 'ok']));
+
 // Serve React index.html for the root
 Route::get('/', function () {
     return 'App is running!';
