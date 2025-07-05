26c5a93a358e775a72c1737ca23955839c4def73
victor juma <77642020+vic657@users.noreply.github.com>
2025-07-05 16:26:07 +0300

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -15,7 +15,7 @@ export default function Login() {
     e.preventDefault();
     setLoading(true);
     try {
-      const res = await axios.post("/api/login", form);
+      const res = await axios.post("/login", form);
 
       localStorage.setItem("token", res.data.token);
       localStorage.setItem("user", JSON.stringify(res.data.user));
